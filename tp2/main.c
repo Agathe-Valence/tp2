25025ecef63b2b0a6dcbbf5f584add0c93405a99
Agathe <valence.agathe.1s3@gmail.com>
2020-10-21 12:31:11 +0200

    Initial Commit

---
+++ tp2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tp2
+//
+//  Created by Travail on 21/10/2020.
+//  Copyright © 2020 Travail. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
